3cd3a7c663fe5d39ce0b2208c55948a792c93b67
Chandler Cheng <msdeme206@gmail.com>
2025-01-30 15:01:27 +0800

    TestCommit

--- ConsoleApplication1/vectorTool.h
+++ ConsoleApplication1/vectorTool.h
@@ -14,3 +14,6 @@ void appendToVector(std::vector<unsigned char>& vec, const std::string& str);
 void appendToVector(std::vector<unsigned char>& vec, std::vector<unsigned char>& vec2);
 
 std::vector<unsigned char> convertToCodePage(const std::vector<unsigned char>& input, UINT codePage);
+
+
+
